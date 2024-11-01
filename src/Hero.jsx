810a3a03e44a27a7034f1d9cfcf2a89f353d2b45
RAY OMIYI <rayikponmwonsaomiyi@gmail.com>
2024-11-01 18:35:55 +0100

    firt commit

---
+++ src/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import heroImg from './assets/hero.svg'
+
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum quam
+            vero fuga sint iure ullam debitis consequuntur itaque ut, tempora,
+            minima nobis natus amet ipsa eos provident impedit corporis veniam
+            in animi distinctio. Necessitatibus, cumque!
+          </p>
+        </div>
+        <div className="img-container">
+            <img src={heroImg} alt="woman and the browser" className="img"/>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
